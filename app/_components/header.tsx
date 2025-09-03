7d78207444a8f55ea38cee588e2598442603d675
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-09-03 00:29:48 -0300

    feat: add sidebar-sheet component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,19 +1,10 @@
-import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react"
+import { MenuIcon } from "lucide-react"
 import Image from "next/image"
-import Link from "next/link"
 
-import { quickSearchOptions } from "../_constants/quick-search"
-import { Avatar, AvatarImage } from "./ui/avatar"
+import { SidebarSheet } from "./sidebar-sheet"
 import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "./ui/sheet"
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 
 export function Header() {
   return (
@@ -27,61 +18,7 @@ export function Header() {
             </Button>
           </SheetTrigger>
           <SheetContent className="overflow-y-auto">
-            <SheetHeader>
-              <SheetTitle className="text-left">Menu</SheetTitle>
-            </SheetHeader>
-
-            <div className="flex items-center gap-3 border-b border-solid py-5">
-              <Avatar>
-                <AvatarImage src="https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-              </Avatar>
-
-              <div className="">
-                <p className="font-bold">Lucas Fernandes</p>
-                <p className="text-xs">lucasfernandes@gmail.com</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-2 border-b border-solid py-5">
-              <SheetClose asChild>
-                <Button className="justify-start gap-2" variant="ghost" asChild>
-                  <Link href="/">
-                    <HomeIcon size={18} />
-                    Início
-                  </Link>
-                </Button>
-              </SheetClose>
-
-              <Button className="justify-start gap-2" variant="ghost">
-                <CalendarIcon size={18} />
-                Agendamentos
-              </Button>
-            </div>
-
-            <div className="flex flex-col gap-2 border-b border-solid py-5">
-              {quickSearchOptions.map((option) => (
-                <Button
-                  key={option.title}
-                  className="justify-start gap-2"
-                  variant="ghost"
-                >
-                  <Image
-                    src={option.imageUrl}
-                    alt={option.title}
-                    height={18}
-                    width={18}
-                  />
-                  {option.title}
-                </Button>
-              ))}
-            </div>
-
-            <div className="flex flex-col gap-2 py-5">
-              <Button variant="ghost" className="justify-start gap-2">
-                <LogOutIcon size={18} />
-                Sair da conta
-              </Button>
-            </div>
+            <SidebarSheet />
           </SheetContent>
         </Sheet>
       </CardContent>
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -5,7 +5,9 @@ import { notFound } from "next/navigation"
 
 import { PhoneItem } from "@/app/_components/phone-item"
 import { ServiceItem } from "@/app/_components/service-item"
+import { SidebarSheet } from "@/app/_components/sidebar-sheet"
 import { Button } from "@/app/_components/ui/button"
+import { Sheet, SheetTrigger } from "@/app/_components/ui/sheet"
 import { db } from "@/app/_lib/prisma"
 
 interface BarbershopPageProps {
@@ -32,8 +34,8 @@ export default async function BarbershopPage({ params }: BarbershopPageProps) {
     <div>
       <div className="relative h-[250px] w-full">
         <Image
+          alt={barbershop.name}
           src={barbershop?.imageUrl}
-          alt={barbershop?.name}
           fill
           className="object-cover"
         />
@@ -49,17 +51,22 @@ export default async function BarbershopPage({ params }: BarbershopPageProps) {
           </Link>
         </Button>
 
-        <Button
-          size="icon"
-          variant="secondary"
-          className="absolute right-4 top-4"
-        >
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              size="icon"
+              variant="outline"
+              className="absolute right-4 top-4"
+            >
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SidebarSheet />
+        </Sheet>
       </div>
-      <div className="border-b border-solid p-5">
-        <h1 className="mb-3 text-xl font-bold">{barbershop?.name}</h1>
 
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
         <div className="mb-2 flex items-center gap-2">
           <MapPinIcon className="text-primary" size={18} />
           <p className="text-sm">{barbershop?.address}</p>
@@ -67,27 +74,27 @@ export default async function BarbershopPage({ params }: BarbershopPageProps) {
 
         <div className="flex items-center gap-2">
           <StarIcon className="fill-primary text-primary" size={18} />
-          <p className="text-sm">5,0 (299 avaliações)</p>
+          <p className="text-sm">5,0 (499 avaliações)</p>
         </div>
       </div>
 
-      <div className="space-y-3 border-b border-solid p-5">
+      <div className="space-y-2 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop?.description}</p>
       </div>
+
       <div className="space-y-3 border-b border-solid p-5">
-        <h2 className="mb-3 text-xs font-bold uppercase text-gray-400">
-          Serviços
-        </h2>
+        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
         <div className="space-y-3">
           {barbershop.services.map((service) => (
             <ServiceItem key={service.id} service={service} />
           ))}
         </div>
       </div>
+
       <div className="space-y-3 p-5">
         {barbershop.phones.map((phone) => (
-          <PhoneItem phone={phone} key={phone} />
+          <PhoneItem key={phone} phone={phone} />
         ))}
       </div>
     </div>
