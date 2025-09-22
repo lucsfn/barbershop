983ca7e06b77a97dca1efdc1b706a0faf1a8616b
Lucas Fernandes <lucasfernandes1506@gmail.com>
2025-09-22 15:11:05 -0300

    feat: ensure booking can be canceled

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -1,16 +1,35 @@
+"use client"
+
 import { format, isFuture } from "date-fns"
 import { ptBR } from "date-fns/locale"
 import Image from "next/image"
+import { useState } from "react"
+import { toast } from "sonner"
 
+import { deleteBooking } from "../_actions/delete-booking"
 import { Prisma } from "../generated/prisma"
 import BookingSummary from "./booking-summary"
 import { PhoneItem } from "./phone-item"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import { Badge } from "./ui/badge"
+import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
 import {
   Sheet,
+  SheetClose,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -31,15 +50,32 @@ interface BookingItemProps {
 }
 
 export function BookingItem({ booking }: BookingItemProps) {
+  const [isSheetOpen, setIsSheetOpen] = useState(false)
   const {
     service: { barbershop },
   } = booking
   const isConfirmed = isFuture(booking.date)
 
+  async function handleCancelBooking() {
+    try {
+      await deleteBooking(booking.id)
+      setIsSheetOpen(false)
+      toast.success("Reserva cancelada com sucesso!")
+    } catch (error) {
+      console.error(error)
+
+      toast.error("Erro ao cancelar reserva. Tente novamente.")
+    }
+  }
+
+  function handleSheetOpenChange(isOpen: boolean) {
+    setIsSheetOpen(isOpen)
+  }
+
   return (
     <>
-      <Sheet>
-        <SheetTrigger className="w-full">
+      <Sheet open={isSheetOpen} onOpenChange={handleSheetOpenChange}>
+        <SheetTrigger className="w-full min-w-[90%]">
           <Card className="min-w-[90%]">
             <CardContent className="flex justify-between p-0">
               <div className="flex flex-col gap-2 py-5 pl-5">
@@ -49,7 +85,9 @@ export function BookingItem({ booking }: BookingItemProps) {
                 >
                   {isConfirmed ? "Confirmado" : "Finalizado"}
                 </Badge>
-                <h3 className="font-semibold">{booking.service.name}</h3>
+                <h3 className="text-left font-semibold">
+                  {booking.service.name}
+                </h3>
                 <div className="flex items-center gap-2">
                   <Avatar className="h-6 w-6">
                     <AvatarImage src={booking.service.barbershop.imageUrl} />
@@ -71,7 +109,7 @@ export function BookingItem({ booking }: BookingItemProps) {
             </CardContent>
           </Card>
         </SheetTrigger>
-        <SheetContent className="w-[90%]">
+        <SheetContent className="w-[85%]">
           <SheetHeader>
             <SheetTitle className="text-left">
               Informações da reserva
@@ -84,7 +122,7 @@ export function BookingItem({ booking }: BookingItemProps) {
               className="rounded-xl object-cover"
               alt={`Mapa da baerbearia ${barbershop.name}`}
             />
-            <Card className="roundend-xl z-50 mx-5 mb-3 w-full">
+            <Card className="z-50 mx-5 mb-3 w-full rounded-xl">
               <CardContent className="flex items-center gap-3 px-5 py-3">
                 <Avatar>
                   <AvatarImage src={barbershop.imageUrl} />
@@ -103,17 +141,57 @@ export function BookingItem({ booking }: BookingItemProps) {
             >
               {isConfirmed ? "Confirmado" : "Finalizado"}
             </Badge>
-            <BookingSummary
-              service={booking.service}
-              barbershop={booking.service.barbershop}
-              selectedDate={booking.date}
-            />
+            <div className="mb-3 mt-6">
+              <BookingSummary
+                service={booking.service}
+                barbershop={booking.service.barbershop}
+                selectedDate={booking.date}
+              />
+            </div>
             <div className="space-y-3">
               {barbershop.phones.map((phone, index) => (
                 <PhoneItem key={index} phone={phone} />
               ))}
             </div>
           </div>
+          <SheetFooter className="mt-6">
+            <div className="flex items-center gap-3">
+              <SheetClose asChild>
+                <Button className="w-full" variant="outline">
+                  Voltar
+                </Button>
+              </SheetClose>
+              {isConfirmed && (
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button className="w-full" variant="destructive">
+                      Cancelar reserva
+                    </Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent className="w-[90%]">
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Você quer cancelar sua reserva?
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        O cancelamento da reserva removerá definitivamente este
+                        agendamento do nosso sistema. Deseja continuar?
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Voltar</AlertDialogCancel>
+                      <AlertDialogAction
+                        variant="destructive"
+                        onClick={handleCancelBooking}
+                      >
+                        Cancelar
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              )}
+            </div>
+          </SheetFooter>
         </SheetContent>
       </Sheet>
     </>
